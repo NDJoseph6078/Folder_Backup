f655aed43670e6dd0e2ed02afb5f546ea28cce09
Nathan Joseph <nathanjoseph6078@gmail.com>
2024-10-23 21:17:27 +0100

    Recreated venv and install necessary modules

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import os, shutil, datetime, time
+from os import path
+import shutil
+import datetime
+import time
 import schedule
 source_dir = None
 target_dir = None
+
+def copy_source_to_destination(source, destination) -> None:
+    today = datetime.date.today()
+    destination_dir: str = path.join(destination, str(today))
+    
